7f0d844018552237d01011e68af983d6253d9b98
nmdispo <nikkimargarettedispo31@gmail.com>
2025-05-19 02:29:19 +0800

    Added First Song Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import MainView from '@/views/layout/MainView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import GagaView from '@/views/layout/GagaView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-     path: '/',
-     name: 'main',
-    component: MainView,
+      path: '/',
+      name: 'main',
+      component: MainView,
     },
-   
+    {
+      path: '/gagaview',
+      name: 'GagaView',
+      component: GagaView,
+    },
   ],
 })
 
